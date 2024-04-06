e309294797b71c7d6a2cac134b070e73346656e9
STARTRACEX <startraceebr@foxmail.com>
2024-04-07 00:22:01 +0800

    Add common package id

---
+++ js/src/common/id.ts
@@ -0,0 +1,1 @@
+export const packageID = 'startracex-flarum-dark';
---
+++ src/ID.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Startracex\FlarumDark;
+
+class ID
+{
+    public static string $packageID = "startracex-flarum-dark";
+}
