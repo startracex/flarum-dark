e66718f0daaaf21e9e5b19b048bf874b122d4eab
STARTRACEX <startraceebr@foxmail.com>
2024-04-09 20:28:00 +0800

    Update keep-dist script

--- js/scripts/keep-dist.cjs
+++ js/scripts/keep-dist.cjs
@@ -1,2 +1,17 @@
 const fs = require('node:fs');
-fs.writeFileSync('.gitignore', '');
+
+const ignoreFile = '.gitignore';
+const content = fs.readFileSync(ignoreFile, 'utf8');
+
+const keepDist = content
+  .trim()
+  .split('\n')
+  .reduce((acc, cur) => {
+    cur = cur.trim();
+    if (cur === 'dist' || cur === 'dist-typings') {
+      cur = `#${cur}`;
+    }
+    return acc + cur + '\n';
+  }, '');
+
+fs.writeFileSync(ignoreFile, keepDist);
